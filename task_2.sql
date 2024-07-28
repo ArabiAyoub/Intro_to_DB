e81314592c23cefd5bff0b8cde1129e033ff10f1
ArabiAyoub <ayoub.arabi.98@gmail.com>
2024-07-28 21:22:51 +0100

    add author name

--- task_2.sql
+++ task_2.sql
@@ -1,25 +1,25 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Create the 'authors' table
-CREATE TABLE authors (
+-- Create the 'Authors' table
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    author_name VARCHAR(100) NOT NULL,
     email VARCHAR(100)
 );
 
--- Create the 'books' table
-CREATE TABLE books (
+-- Create the 'Books' table
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     author_id INT,
     genre VARCHAR(50),
     price DECIMAL(10, 2),
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Create the 'customers' table
-CREATE TABLE customers (
+-- Create the 'Customers' table
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -27,22 +27,22 @@ CREATE TABLE customers (
     address VARCHAR(255)
 );
 
--- Create the 'orders' table
-CREATE TABLE orders (
+-- Create the 'Orders' table
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     status VARCHAR(20),
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create the 'order_details' table
-CREATE TABLE order_details (
+-- Create the 'Order_Details' table
+CREATE TABLE Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity INT,
     price_each DECIMAL(10, 2),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
