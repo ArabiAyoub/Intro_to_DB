534b8b1e0cd88d2dfc44827565eae70baaff9613
ArabiAyoub <ayoub.arabi.98@gmail.com>
2024-07-28 21:53:38 +0100

    inist

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
--- This script provides a full description of the 'books' table using information from the INFORMATION_SCHEMA.COLUMNS table.
+-- This script provides a full description of the 'Books' table using information from the INFORMATION_SCHEMA.COLUMNS table.
 -- Ensure that the database context is set to 'alx_book_store' when executing this script.
 
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+SELECT COLUMN_NAME, COLUMN_TYPE, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
